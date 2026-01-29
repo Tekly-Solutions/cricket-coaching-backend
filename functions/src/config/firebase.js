e56ab04d315ae76c5fb7d1fa36b4fd770e3a9341
Dilukshan <dadilukshan@gmail.com>
2026-01-29 16:48:24 +0530

    remove service account file

--- functions/src/config/firebase.js
+++ functions/src/config/firebase.js
@@ -1,8 +1,17 @@
-import admin from "firebase-admin";
-import serviceAccount from "./service-account.js";
+// import admin from "firebase-admin";
+// import serviceAccount from "./service-account.js";
 
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-});
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+// });
+
+// export default admin;
+
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp();
+}
 
 export default admin;
+
